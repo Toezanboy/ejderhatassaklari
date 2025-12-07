cf936aab32bbd8adef65914b2afbabc901b4e27c
Pulwra <bdemi@DESKTOP-0GD9VHK>
2025-12-07 22:09:27 +0300

    test

---
+++ src/main/java/net/mcreator/dragonmine/procedures/NProcedure.java
@@ -0,0 +1,28 @@
+package net.mcreator.dragonmine.procedures;
+
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
+import net.minecraft.world.entity.Entity;
+
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
+public class NProcedure {
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent event) {
+		execute(event, event.getPlayer());
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		entity.setSprinting(true);
+	}
+}
